6b4d42ec19193bd9796e26aea30cdc69834def6f
TCA <tomek.chady@gmail.com>
2025-03-29 13:56:12 +0100

    Doc install

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # neuroforgelab
+
+## Install
+
+Due to the unique requirements of Isaac Lab, the recommended way of installing
+is to use the [```install.sh```](./install.sh) script. It ensures
+```python3.10``` is installed, compile requirements are installed, installs
+this and Isaac Lab.
--- install.sh
+++ install.sh
@@ -5,18 +5,9 @@ then
     if command -v apt-get &> /dev/null
     then
         sudo apt-get install python3.10
-    elif command -v brew &> /dev/null
-    then
-        brew install python@3.10
     elif command -v dnf &> /dev/null
     then
         sudo dnf install python3.10 libxcrypt-compat
-    elif command -v pacman &> /dev/null
-    then
-        sudo pacman -S python3.10
-    elif command -v zypper &> /dev/null
-    then
-        sudo zypper install python3.10
     else
         echo "Python 3.10 not found. Please install Python 3.10"
         exit
