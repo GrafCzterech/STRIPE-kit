44bc18e27f3c862d5092f37320139cf4dd86d6c1
TCA <tomek.chady@gmail.com>
2025-04-14 17:32:48 +0200

    Import fix

--- src/neuroforgelab/__init__.py
+++ src/neuroforgelab/__init__.py
@@ -1,11 +1,12 @@
 from .terrain import TerrainInstance
 from .scene_spec import SceneSpec
 from .factory import SceneCfgFactory
-from .asset import AssetSpec, AssetInstance
+from .asset import AssetSpec, AssetInstance, IdenticalAssetSpec
 
 __all__ = [
     "SceneCfgFactory",
     "AssetSpec",
+    "IdenticalAssetSpec",
     "TerrainInstance",
     "AssetInstance",
     "SceneSpec",
